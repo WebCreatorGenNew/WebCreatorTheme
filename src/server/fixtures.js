752a8a22e7e121e483154cbef7d1ff2c6c85c17e
Nghiep TO <tovannghiep2310@gmail.com>
2016-04-23 10:37:56 +0700

    Add default menu

---
+++ src/server/fixtures.js
@@ -0,0 +1,10 @@
+Meteor.startup(function () {
+  // Insert sample data if the student collection is empty
+  if (Menus.find().count() === 0) {
+  	Menus.insert({ _id: "1", title: "Home" });
+  	Menus.insert({ _id: "2", title: "About" });
+  	Menus.insert({ _id: "3", title: "Our Menu"});
+  	Menus.insert({ _id: "4", title: "Contact"});
+  }
+});
+ 
